d76de555f1f2cfd9c117621b5d5070225be1623f
Rahul Agrawal <rahulag0110@gmail.com>
2023-11-29 01:10:04 +0900

    Updated gpt for diet and spice level.

--- LLM_Recipe/prompt_generator.py
+++ LLM_Recipe/prompt_generator.py
@@ -7,13 +7,15 @@ from langchain.prompts import (
 
 system_message_template = """
 You are a CookieGPT, helpful AI assistant that provides help with suggesting recipes and help a user cook based on following information:
-{pantry} : These are the ingedrients and their respective expiration dates (guess if not specified) user has in their pantry. Suggest a recipie with expiring ingedrient first. You don't need to use up all the ingedrients.
-{timeOfDay}, {cuisine} : Here is the time of the day and cuisine/region/style user wants to cook. If its none you are free to suggest any recipe.
-{numberOfRecipes}, {numberOfServings} : Number of recipies you suggest and proprtion of the recipe.
-If any of the above information is not provided, you are free to assume it.
-
+{pantry} : These are the ingedrients and their respective expiration dates (guess if not specified) user has in their pantry. Suggest a recipie with expiring ingedrient first. You don't need to use up all the ingedrients but the recipe you suggest should not contain any ingedrients other than these.
+Diet ststus - {diet} : This is the diet user is following. It can be either strict diet (low carbs and fat) or nomal diet.
+Spice Level - {spiceLevel}
 Output a list of recipes. Each recipe should be strictly a JSON with the following keys: recipeName, ingedrientsUsed, servings, steps. So the output should be in the following format: [JSON, JSON, JSON, ...]. Don't include any greeting or any extra information in the output.
 """
+
+# {timeOfDay}, {cuisine} : Here is the time of the day and cuisine/region/style user wants to cook. If its none you are free to suggest any recipe.
+# {numberOfRecipes}, {numberOfServings} : Number of recipies you suggest and proprtion of the recipe.
+
 
 human_message_template = "Don't have any greetins or any extra information in the output. Just follow the format very carefully."
 ai_message_template = "{ai_text}"
@@ -40,5 +42,5 @@ outputFormat = """ [{
 }, {...}, {...}] """
 
 def prompt_generator(input, outputFormat, chatInput):
-    prompt = chat_prompt.format_prompt(pantry = input["pantry"], timeOfDay = input["timeOfDay"], cuisine = input["cuisine"], numberOfRecipes = input["numberOfRecipes"], numberOfServings = input["numberOfServings"], outputFormat = outputFormat, chatInput = chatInput).to_messages()
+    prompt = chat_prompt.format_prompt(pantry = input["pantry"], diet = input["diet"], spiceLevel = input["spiceLevel"], outputFormat = outputFormat, chatInput = chatInput).to_messages()
     return prompt
--- example1.py
+++ example1.py
@@ -1,20 +1,19 @@
 from LLM_Recipe.return_recipe import return_recipe
 input = {
     "pantry": "[chicken drumsticks, chives, carrots, potatoes, tomatoes, onions, garlic, mayonaise, corn, papaya]",
-    "timeOfDay": "Dinner",
-    "cuisine": "Any",
-    "numberOfRecipes": 2,
-    "numberOfServings": 3,    
+    "diet": "strict diet",
+    "spiceLevel": "medium"   
 }
-outputFormat = """ [{
-    "recipeName": "Name of the recipe",
-    "ingedrientsUsed": "Ingredients used in the recipe",
-    "servings": "Number of servings",
-    "steps": "Steps to cook the recipe mentioned clearly in a numbered list",
-}, {...}, {...}] """
-recipe = return_recipe(input, outputFormat, "")
+outputFormat = """ [
+{"recipeName": "Name of the recipe", "ingedrientsUsed": "Ingredients used in the recipe", "servings": "Number of servings", "steps": "Steps to cook the recipe mentioned clearly in a numbered list"},
+{...},
+{...}
+] """
+recipe = return_recipe(input, outputFormat, "I want something easy and quick to make.")
 
 #extracting content from the recipe
 answer = recipe.content
 
+with open("example1.txt", 'w') as file:
+    file.write(answer)
 print(answer)
